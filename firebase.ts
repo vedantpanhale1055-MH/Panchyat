6075ede93d7e603319c10e293200755889666aae
Vedant Panhale <vedantpanhale.01@gmail.com>
2026-04-04 23:58:03 +0530

    fix: moved firebase keys to env file

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJ9E3q2UVpkQokssYOPZ0AM0fbE0VcXmY",
-  authDomain: "panchyat-ce15b.firebaseapp.com",
-  projectId: "panchyat-ce15b",
-  storageBucket: "panchyat-ce15b.firebasestorage.app",
-  messagingSenderId: "22302207646",
-  appId: "1:22302207646:web:b3b277bc64547eebd6d58f",
-  measurementId: "G-2E77F5LZKK"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
